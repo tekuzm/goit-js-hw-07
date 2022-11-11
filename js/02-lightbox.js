788bed0e214ef44b3d06f2a1ac4529f68f8647cb
TetianaKuzma <tetiana.b97@gmail.com>
2022-11-11 19:29:02 +0200

    task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const galleryEls = galleryItems
+  .map(
+    ({ preview, description, original }) =>
+      `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+      </a>`
+  )
+  .join("");
+
+galleryList.insertAdjacentHTML("beforeend", galleryEls);
+
+new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+});
