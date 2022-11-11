1a2d76dcc254c2816de81d31bb516c0e55925083
TetianaKuzma <tetiana.b97@gmail.com>
2022-11-11 15:55:16 +0200

    task 1

---
+++ js/01-gallery.js
@@ -0,0 +1,51 @@
+import { galleryItems } from "./gallery-items.js";
+
+// Change code below this line
+const galleryList = document.querySelector(".gallery");
+
+const galleryEls = galleryItems
+  .map(
+    ({ preview, description, original }) =>
+      `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`
+  )
+  .join("");
+
+galleryList.insertAdjacentHTML("beforeend", galleryEls);
+galleryList.addEventListener("click", onClickGalleryList);
+galleryList.addEventListener("keydown", onEscKeyPress);
+
+let instance;
+
+function onClickGalleryList(e) {
+  e.preventDefault();
+  const isImageEl = e.target.classList.contains("gallery__image");
+
+  if (!isImageEl) {
+    return;
+  }
+
+  const imageUrl = e.target.dataset.source;
+
+  instance = basicLightbox.create(`
+		<img src="${imageUrl}">`);
+
+  instance.show();
+}
+
+function onEscKeyPress(e) {
+  const ESC_KEY_CODE = "Escape";
+  const isEscKey = e.code === ESC_KEY_CODE;
+
+  if (isEscKey) {
+    instance.close();
+  }
+}
